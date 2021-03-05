0e2a2864fd9b2713324c1c92c07572d5b3892b45
Acovid <acovid@gmail.com>
2021-03-05 12:44:14 +0100

    Add API route

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://72bee974.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
